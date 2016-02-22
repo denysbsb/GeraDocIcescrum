06482154ab3a14d2dc36ace12565141819b1f503
denysbsb <denys.design@gmail.com>
2016-02-22 17:55:39 -0300

    request com retorno

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var Docxtemplater = require('docxtemplater');
 var ImageModule = require('docxtemplater-image-module');
 var EasyZip = require('easy-zip').EasyZip;
+var request = require('request');
 
 //Metedo para exibixao de imagens
 var opts = {}
@@ -25,14 +26,34 @@ var doc = new Docxtemplater(content);
  	doc.attachModule(imageModule)
     doc.load(content)
 
-//set Variaveis no Template
-	doc.setData({
+    request({
+    headers: {
+      'Content-Type': 'application/json'
+    },
+     'auth': {
+    'user': 'denys.araujo',
+    'pass': 'a1920212223',
+    'sendImmediately': false
+  	},
+    uri: 'http://177.69.58.70:8888/icescrum/ws/p/TCUPUSH/story',
+    method: 'GET'
+  }, function (err, res, body) {
+		console.log(body); 
+  });
+
+  
+    //console.log(this.req._callback);
+
+
+ 	var dados = {
 	    "first_name":"Denys",
 	    "last_name":"Edgar",
 	    "phone":"0613399-2325",
 	    "description":"Nova servico",
 	    image:"images/image.png"
-	});
+	};
+//set Variaveis no Template
+	 doc.setData(dados);
 
 //aplica os valores no Template
 	doc.render();

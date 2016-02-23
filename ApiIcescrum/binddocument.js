026dfe7b1d6c0a3257111cf8b846670945fd91ec
denysbsb <denys.design@gmail.com>
2016-02-23 18:43:44 -0300

    Deletar arquivos antigos

--- ApiIcescrum/binddocument.js
+++
@@ -1,9 +0,0 @@
-var api = require('./icescrumapi.js');
-
-if(process.argv[2] === undefined){
-	console.log('Nome do projeto é requerido');
-	return;
-}
-
-api.request(process.argv[2]);
-console.log(api.request(process.argv[2]));
